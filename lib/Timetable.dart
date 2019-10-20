2619d21e175a6131fcc0416c0d951b2efff2eb66
Wojciech Błądek <wjbladek@gmail.com>
2019-10-21 01:12:38 +0200

    generally working timetable prototype

--- lib/Timetable.dart
+++ lib/Timetable.dart
@@ -1,7 +1,10 @@
 // import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'main.dart';
+import 'City.dart';
+import 'custom_expansion_tile.dart' as Custom;
 
 class TimetablePage extends StatefulWidget {
   @override
@@ -9,65 +12,199 @@ class TimetablePage extends StatefulWidget {
 }
   
 class _TimetablePageState extends State<TimetablePage> {
-  Future<List<Performance>> performances = MyAppState().performances;
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FutureBuilder<List<Performance>>(
-        future: performances,
-        // future: fetchTimetableData('http://grzybek.bymarcin.com:8081/getAll'),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          // switch (snapshot.connectionState) {
-
-          //   case ConnectionState.none:
-          //     // TODO: Handle this case.
-          //     break;
-          //   case ConnectionState.waiting:
-          //     // TODO: Handle this case.
-          //     break;
-          //   case ConnectionState.active:
-          //     // TODO: Handle this case.
-          //     break;
-          //   case ConnectionState.done:
-          //     // TODO: Handle this case.
-          //     break;
-          // }
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int i) {
-                return Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(snapshot.data[i].problem),
-                        Text(snapshot.data[i].age),
-                        Text(snapshot.data[i].stage),
-                      ],
-                    ),
-                    Row( 
-                      children: [
-                        Text(snapshot.data[i].performance),
-                        Text(snapshot.data[i].team),
-                        Text(snapshot.data[i].spontan),
-                        // Text(snapshot.data[i].id.toString()),
-                        // Text(snapshot.data[i].city),
-                      ]
-                    ),
-                    Divider(),
-                  ]
-                );
-              }
-            );
-          }
-          else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        }
+      child: Column(
+        children: [
+          ListTile(
+            title:  TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Szukaj',
+                ),
+              ),
+            trailing: Icon(Icons.filter_list),
+          ),
+          // _rawtimetable(),
+          _timetable(),
+          // _playBlock(),
+          // _playItem(),
+        ]
       )
     );
-  }  
-}
+  }
+  Widget _rawtimetable() {
+    final chsnProvider = Provider.of<ChosenCity>(context);
+    return FutureBuilder<List<Performance>>(
+            future: Storage('timeTableGetAll.json').readFileTT(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                var dataSubset = snapshot.data.where(((p) => p.city == chsnProvider.chosenCity)).toList();
+                // print('Podzbiór danych: ${dataSubset[0].city}');
+                // print('Wybrane miasto: ${chsnProvider.chosenCity}');
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: 1,
+                    itemBuilder: (BuildContext context, int i) {
+                      return Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(dataSubset[i].problem),
+                              Text(dataSubset[i].age),
+                              Text(dataSubset[i].stage),
+                            ],
+                          ),
+                          Row( 
+                            children: [
+                              Text(dataSubset[i].play),
+                              Text(dataSubset[i].team),
+                              Text(dataSubset[i].spontan),
+                              // Text(dataSubset.[i].id.toString()),
+                              // Text(dataSubset.[i].city),
+                            ]
+                          ),
+                          Divider(),
+                        ]
+                      );
+                    }
+                  ),
+                );
+              }
+              else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            }
+          );
+  }
+  Widget _timetable() {
+    final chsnProvider = Provider.of<ChosenCity>(context);
+
+    return FutureBuilder<List<Performance>>(
+            future: Storage('timeTableGetAll.json').readFileTT(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                var dataSubset = snapshot.data.where(((p) => p.city == chsnProvider.chosenCity)).toList();
+                var problems = new List();
+                var ages = new List();
+                var stages = new List();
+                for (var p in dataSubset) {
+                  problems.add(p.problem);
+                  ages.add(p.age);
+                  stages.add(p.stage);
+                }
+                var problemsPresent = Set.of(problems);
+                var agesPresent = Set.of(ages);
+                var stagesPresent = Set.of(stages);
+                List<Widget> blockList = new List<Widget>();
+                // print('\n$problemsPresent\n $agesPresent\n$stagesPresent');
+                for (var pr in problemsPresent) {
+                  for (var ag in agesPresent) {
+                    for (var st in stagesPresent) {
+                      List blockData = dataSubset.where(
+                        (p) =>
+                        p.problem == pr &&
+                        p.age == ag &&
+                        p.stage == st
+                      ).toList();
+                      if (blockData.isNotEmpty) {
+                        print('$pr, $ag, $st');
+                        blockList.add(_playBlock(blockData));
+                      }
+                      // return _playBlock(blockData);
+                      // return Text('data');
+                    }
+                  }
+                }
+                      // return null;
+                return Expanded(
+                  // child: ListView.builder(
+                    // itemCount: 2,
+                    // itemBuilder: (BuildContext context, int i) {
+                    child: ListView(
+                      children: <Widget>[...blockList],
+                      // children: <Widget>[Text('lll')],
+                    )
+                );
+
+                  //   }
+                  // ),
+                // );
+              }
+              else if (snapshot.hasError) {
+
+                return Text("${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            }
+          );
+  }
+  Widget _playBlock(List<Performance> blockData) {
+    List<Widget> playList = new List<Widget>();
+    for (var perf in blockData) playList.add(_playItem(perf));
+    return Custom.ExpansionTile(
+      // backgroundColor: Colors.teal[300],
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text('Problem:'),
+              Text(blockData[0].problem.substring(9,10)),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text('Kategoria\nwiekowa:'),
+              Text(blockData[0].age.substring(15)),
+            ],
+          ),      
+          Column(
+            children: <Widget>[
+              Text('Scena:'),
+              Text(blockData[0].stage.substring(6,7)),
+            ],
+          ),
+        ],
+    ),
+    children: <Widget>[
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Text('P'),
+          Text('Drużyna'),
+          Text('S'),
+        ],
+      ),
+      ...playList,
+    ],
+    );
+  }
+Widget _playItem(Performance performance) {
+    return ListTile(
+      dense: true,
+      title: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Text(performance.play)),
+          Expanded(
+            flex: 10,
+            child: Text(performance.team, textAlign: TextAlign.center,)),
+          Expanded(
+            flex: 2,
+            child: Text(performance.spontan)),
+        ]
+      ),
+      trailing: Icon(Icons.favorite_border, size: 12.0),
+      // subtitle: ,
+
+    );
+  }
+
+  // List dataSubdivision(List dataSubset) {
     
-// class
+  // }
+}
