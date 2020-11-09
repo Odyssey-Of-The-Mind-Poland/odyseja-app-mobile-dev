22fb22ad6afa3ebdd26602b5eb9897518f305266
Wojciech Błądek <wjbladek@gmail.com>
2020-11-09 02:50:19 +0100

    Add missing changeCityCommand

---
+++ lib/commands/change_city_command.dart
@@ -0,0 +1,15 @@
+import 'package:ootm_app/data/city.dart';
+import 'base_command.dart';
+
+
+class ChangeCityCommand extends BaseCommand {
+
+
+  Future<void> change(City newCity) async {
+    await cityDataModel.closeCityDatabase();
+    cityDataModel.chosenCity = newCity;
+    await cityDataModel.openCityDatabase();
+    await cityDataModel.loadCityDatabase();
+  }
+  
+}
