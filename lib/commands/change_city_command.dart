f2970022b401d154c10e07fb47f2bf4a3136bc5a
Wojciech Błądek <wjbladek@gmail.com>
2021-02-21 21:14:25 +0100

    Fix favourites to be city-aware

--- lib/commands/change_city_command.dart
+++ lib/commands/change_city_command.dart
@@ -1,15 +1,12 @@
 import 'package:ootm_app/data/city.dart';
 import 'base_command.dart';
 
-
 class ChangeCityCommand extends BaseCommand {
-
-
   Future<void> change(City newCity) async {
     await cityDataModel.closeCityDatabase();
     cityDataModel.chosenCity = newCity;
     await cityDataModel.openCityDatabase();
     await cityDataModel.loadCityDatabase();
+    favModel.favList = cityDataModel.pfGroups;
   }
-  
 }
